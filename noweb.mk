96715e737162537aab2dce5fe5f97559930cffa4
Daniel Bosk <dbosk@kth.se>
2016-10-22 23:42:22 +0200

    Adds documented version of noweb.mk{,nw}

--- noweb.mk
+++ noweb.mk
@@ -1,28 +1,24 @@
-.SUFFIXES: .nw
+ifndef NOWEB_MK
+NOWEB_MK = true
 
-# implicit rules for C and C++ code
-.SUFFIXES: .c.nw .cpp.nw .cxx.nw
-.SUFFIXES: .c .cpp .cxx
-.nw.c .c.nw.c .nw.cpp .cpp.nw.cpp .nw.cxx .cxx.nw.cxx: noweb
-	notangle -R$@ -L $< | cpif $@
+NOWEB_WEAVE_FLAGS?= -x -n -delay -t2
+NOWEB_TANGLE_FLAGS?= -t2
+.SUFFIXES: .nw .tex .py.nw .c.nw .h.nw .cpp.nw .hpp.nw .mk.nw
+.nw.tex .py.nw.tex .c.nw.tex .h.nw.tex .cpp.nw.tex .hpp.nw.tex .mk.nw.tex:
+	noweave ${NOWEB_WEAVE_FLAGS} $< > $@
+.SUFFIXES: .nw .c.nw .cpp.nw .cxx.nw .c .cpp .cxx
+.nw.c .c.nw.c .nw.cpp .cpp.nw.cpp .nw.cxx .cxx.nw.cxx:
+	notangle ${NOWEB_TANGLE_FLAGS} -L -R$@ $< | cpif $@
 
-# implicit rules for C and C++ headers
 .SUFFIXES: .h .hpp .hxx
-.nw.h .c.nw.h .nw.hpp .cpp.nw.hpp .nw.hxx .cxx.nw.hxx: noweb
-	notangle -R$@ $< | cpif $@
-
-# implicit rules for Python
-.SUFFIXES: .py.nw
-.SUFFIXES: .py
-.nw.py .py.nw.py: noweb
-	notangle -R$@ $< | cpif $@
-
+.nw.h .c.nw.h .nw.hpp .cpp.nw.hpp .nw.hxx .cxx.nw.hxx:
+	notangle ${NOWEB_TANGLE_FLAGS} -L -R$@ $< | cpif $@
+.SUFFIXES: .nw .py.nw .py
+.nw.py .py.nw.py:
+	notangle ${NOWEB_TANGLE_FLAGS} -R$@ $< | cpif $@
 # implicit rules for make(1) includes
-.SUFFIXES: .mk.nw
-.SUFFIXES: .mk
-.nw.mk .mk.nw.mk: noweb
-	notangle -t2 -R$@ $< | cpif $@
+.SUFFIXES: .nw .mk.nw .mk
+.nw.mk .mk.nw.mk:
+	notangle ${NOWEB_TANGLE_FLAGS} -R$@ $< | cpif $@
 
-
-INCLUDE_MAKEFILES?=.
-include ${INCLUDE_MAKEFILES}/depend.mk
+endif
