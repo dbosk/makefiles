a56ddb99ba56fc379b2a56cf6f21f2be1bd58fa2
Daniel Bosk <dbosk@kth.se>
2016-11-11 01:37:31 +0100

    Adds documented version of package.mk{,nw}
    This also slightly improves naming and functionality. Specifically,
    ensures that several sub-packages can share one tarball.

--- Makefile
+++ Makefile
@@ -61,15 +61,15 @@ miun: ${MIUNFILES}
 PKG_PACKAGES?= 			main miun
 
 PKG_PREFIX=				/usr/local
-PKG_DIR=				/include
+PKG_INSTALL_DIR=		/include
 
 PKG_NAME-main= 			makefiles
-PKG_FILES-main= 		${MKFILES}
-PKG_TARBALL_FILES-main= ${PKG_FILES-main} ${OTHERS} Makefile README.md
+PKG_INSTALL_FILES-main=	${MKFILES}
+PKG_TARBALL_FILES-main=	${PKG_INSTALL_FILES-main} ${OTHERS} Makefile README.md
 
 PKG_NAME-miun=			build-all
-PKG_FILES-miun= 		${MIUNFILES}
-PKG_TARBALL_FILES-miun= ${PKG_FILES-miun} Makefile README.md
+PKG_INSTALL_FILES-miun=	${MIUNFILES}
+PKG_TARBALL_FILES-miun=	${PKG_INSTALL_FILES-miun} Makefile README.md
 
 PUB_SITES?= 			miun
 
--- package.mk
+++ package.mk
@@ -1,123 +1,92 @@
-# $Id$
-# Author: Daniel Bosk <daniel.bosk@miun.se>
-
 ifndef PACKAGE_MK
 PACKAGE_MK=true
 
-# specify all packages
-PKG_PACKAGES?= 			main
-
-PKG_NAME?=				${PACKAGE}
-PKG_FILES?=				${PACKAGE_FILES} ${INSTALL_FILES}
-PKG_PREFIX?=			${PREFIX}
-PKG_DIR?=				${INSTALLDIR}
-
-IGNORE_FILES?=			\(\.svn\|\.git\|CVS\)
-PKG_IGNORE?=			${IGNORE_FILES}
-
-# these three variables should be set in $PACKAGE/Makefile for each package
+PKG_NAME?=          ${PACKAGE}
+PKG_INSTALL_FILES?= ${INSTALL_FILES}
+PKG_TARBALL_FILES?= ${PACKAGE_FILES} ${PKG_INSTALL_FILES}
+IGNORE_FILES?=      \(\.svn\|\.git\|CVS\)
+PKG_IGNORE?=        ${IGNORE_FILES}
+PKG_PREFIX?=        ${PREFIX}
+PKG_INSTALL_DIR?=   ${INSTALLDIR}
+PKG_PACKAGES?=            main
 define variables
-PKG_NAME-$(1)?= 		${PKG_NAME}
-PKG_FILES-$(1)?= 		${PKG_FILES}
-PKG_PREFIX-$(1)?=		${PKG_PREFIX}
-PKG_DIR-$(1)?= 			${PKG_DIR}
+PKG_NAME-$(1)?= 		      ${PKG_NAME}
+PKG_INSTALL_FILES-$(1)?= 	${PKG_INSTALL_FILES}
+PKG_PREFIX-$(1)?=         ${PKG_PREFIX}
+PKG_INSTALL_DIR-$(1)?=    ${PKG_INSTALL_DIR}
 
-# these will usually not need any changes
-PKG_TARBALL-$(1)?=		${PKG_NAME-$(1)}.tar.gz
-PKG_TARBALL_FILES-$(1)?=${PKG_FILES-$(1)}
-PKG_IGNORE-$(1)?= 		${PKG_IGNORE}
+PKG_TARBALL-$(1)?=        ${PKG_NAME-$(1)}.tar.gz
+PKG_TARBALL_FILES-$(1)?=  ${PKG_TARBALL_FILES}
+PKG_IGNORE-$(1)?=         ${PKG_IGNORE}
 endef
-
-# set up default values for all packages, for which values are not already set
 $(foreach pkg,${PKG_PACKAGES},$(eval $(call variables,${pkg})))
-
-
-MKDIR?=		mkdir -p
-CP?=		cp -R
-RM?=		rm -Rf
-TAR=		pax -wzLx ustar
-UNTAR=		pax -rz
+MKDIR?=     mkdir -p
+CP?=        cp -R
+RM?=        rm -Rf
+TAR=        pax -wzLx ustar
+UNTAR=      pax -rz
 ifneq (${MAKE},gmake)
-INSTALL?=	install -Dp
+INSTALL?=   install -Dp
 else
-INSTALL?=	install -CSp
+INSTALL?=   install -CSp
 endif
-
-.PHONY: all
-define all
-all: ${PKG_TARBALL-$(1)} ${PKG_FILES-$(1)}
+PKG_REGEX?=     "|^(.*)$$$$|\1|p"
+$(foreach pkg,${PKG_PACKAGES},$(eval PKG_REGEX-${pkg}?=${PKG_REGEX}))
+.PHONY: all package
+define all_and_package
+all:      ${PKG_TARBALL-$(1)}
+package:  ${PKG_TARBALL-$(1)}
 endef
-$(foreach pkg,${PKG_PACKAGES},$(eval $(call all,${pkg})))
-
+$(foreach pkg,${PKG_PACKAGES},$(eval $(call all_and_package,${pkg})))
 define tarball
-${PKG_TARBALL-$(1)}: ${PKG_TARBALL_FILES-$(1)} pax
-	[ -n "${PKG_TARBALL_FILES-$(1)}" ] && \
-		find ${PKG_TARBALL_FILES-$(1)} -type f -or -type l | \
-		xargs ${TAR} -f $$@ \
-		-s "|^.*/${PKG_IGNORE-$(1)}/.*$$$$||p" \
-		-s "|^\(.*\).export$$$$|${PKG_NAME-$(1)}/\1|p" \
-		-s "|^\(.*\)\.export\([^.]*\)$$$$|${PKG_NAME-$(1)}/\1\.\2|p" \
-		-s "|^|${PKG_NAME-$(1)}/|p"
+${PKG_TARBALL-$(1)}: ${PKG_TARBALL_FILES-$(1)}
 endef
 $(foreach pkg,${PKG_PACKAGES},$(eval $(call tarball,${pkg})))
-
-.PHONY: package
-package:
-
-define package
-package: ${PKG_TARBALL-$(1)}
-endef
-$(foreach pkg,${PKG_PACKAGES},$(eval $(call package,${pkg})))
-
-.PHONY: clean-package
-clean-package:
-
+$(sort $(foreach pkg,${PKG_PACKAGES},${PKG_TARBALL-${pkg}})):
+	[ -n "$^" ] && \
+	  find $^ -type f -or -type l \
+	  | xargs ${TAR} -f $@ \
+	    $(foreach regex,${PKG_REGEX-$(1)},-s ${regex}) \
+	    -s "|^.*/${PKG_IGNORE-$(1)}/.*$$$$||p" \
+	    -s "|^|${PKG_NAME-$(1)}/|p"
+.PHONY: clean clean-package
+clean: clean-package
 define clean-package
+.PHONY: clean-package-$(1)
 clean-package: clean-package-$(1)
 clean-package-$(1):
 	${RM} ${PKG_TARBALL-$(1)}
 endef
 $(foreach pkg,${PKG_PACKAGES},$(eval $(call clean-package,${pkg})))
-
-.PHONY: clean
-clean: clean-package
-
 .PHONY: install
 install: post-install
-
-.PHONY: pre-install
-pre-install:
-
-define pre-install
-pre-install: pre-install-$(1)
-pre-install-$(1):
+.PHONY: pre-install do-install post-install
+post-install: do-install
+do-install:   pre-install
+pre-install:  ${PKG_INSTALL_FILES}
+define post-install
+.PHONY: post-install-$(1)
+post-install: post-install-$(1)
+post-install-$(1): do-install-$(1)
 endef
-$(foreach pkg,${PKG_PACKAGES},$(eval $(call pre-install,${pkg})))
-
-.PHONY: do-install
-do-install: pre-install
+$(foreach pkg,${PKG_PACKAGES},$(eval $(call post-install,${pkg})))
 
 define do-install
+.PHONY: do-install-$(1)
 do-install: do-install-$(1)
-do-install-$(1): pre-install-$(1) ${PKG_FILES-$(1)}
-	${MKDIR} ${PKG_PREFIX-$(1)}${PKG_DIR-$(1)}/
-	for f in ${PKG_FILES-$(1)}; do \
-		[ -d "$$$$f" ] || ${INSTALL} "$$$$f" ${PKG_PREFIX-$(1)}${PKG_DIR-$(1)}/; \
+do-install-$(1): pre-install-$(1)
+	${MKDIR} ${PKG_PREFIX-$(1)}${PKG_INSTALL_DIR-$(1)}/
+	for f in ${PKG_INSTALL_FILES-$(1)}; do \
+	  [ -d "$$$$f" ] || ${INSTALL} "$$$$f" ${PKG_PREFIX-$(1)}${PKG_INSTALL_DIR-$(1)}/; \
 	done
 endef
 $(foreach pkg,${PKG_PACKAGES},$(eval $(call do-install,${pkg})))
 
-.PHONY: post-install
-post-install: do-install
-
-define post-install
-post-install: post-install-$(1)
-post-install-$(1):
+define pre-install
+.PHONY: pre-install-$(1)
+pre-install: pre-install-$(1)
+pre-install-$(1): ${PKG_INSTALL_FILES-$(1)}
 endef
-$(foreach pkg,${PKG_PACKAGES},$(eval $(call post-install,${pkg})))
-
-
-INCLUDE_MAKEFILES?= .
-include ${INCLUDE_MAKEFILES}/export.mk
+$(foreach pkg,${PKG_PACKAGES},$(eval $(call pre-install,${pkg})))
 
 endif
