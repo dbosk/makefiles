357bcd88cbea4c7019eab1bfae2c644f8b9e146a
Daniel Bosk <dbosk@kth.se>
2016-12-13 11:23:03 +0100

    noweb.mk: Generalizes the code
    The code is now more flexible and it follows the make(1) convention of
    having NOTANGLE.suf contain the command-line to tangle files of suffix
    .suf.

--- noweb.mk
+++ noweb.mk
@@ -1,27 +1,28 @@
 ifndef NOWEB_MK
 NOWEB_MK = true
 
-NOWEB_WEAVE_FLAGS?= -x -n -delay -t2
-NOWEB_TANGLE_FLAGS?= -t2
-.SUFFIXES: .nw .tex .py.nw .c.nw .h.nw .cpp.nw .hpp.nw .mk.nw
-.nw.tex .py.nw.tex .c.nw.tex .h.nw.tex .cpp.nw.tex .hpp.nw.tex .mk.nw.tex:
-	noweave ${NOWEB_WEAVE_FLAGS} $< > $@
-.SUFFIXES: .nw .c.nw .cpp.nw .cxx.nw .c .cpp .cxx
-.nw.c .c.nw.c .nw.cpp .cpp.nw.cpp .nw.cxx .cxx.nw.cxx:
-	notangle ${NOWEB_TANGLE_FLAGS} -L -R$@ $< | cpif $@
+NOWEAVE?=       noweave
+NOWEAVEFLAGS?=  -x -n -delay -t2
+NOTANGLE?=      notangle
+NOTANGLEFLAGS?= -t2
+CPIF?=          cpif
 
-.SUFFIXES: .h .hpp .hxx
-.nw.h .c.nw.h .nw.hpp .cpp.nw.hpp .nw.hxx .cxx.nw.hxx:
-	notangle ${NOWEB_TANGLE_FLAGS} -L -R$@ $< | cpif $@
-.SUFFIXES: .hs.nw .hs
-.nw.hs .hs.nw.hs:
-	notangle ${NOWEB_TANGLE_FLAGS} -L -R$@ $< | cpif $@
-.SUFFIXES: .nw .py.nw .py
-.nw.py .py.nw.py:
-	notangle ${NOWEB_TANGLE_FLAGS} -R$@ $< > $@
-# implicit rules for make(1) includes
-.SUFFIXES: .nw .mk.nw .mk
-.nw.mk .mk.nw.mk:
-	notangle ${NOWEB_TANGLE_FLAGS} -R$@ $< > $@
+NOWEB_SUFFIXES+=    .hs
+NOTANGLEFLAGS.hs?=  ${NOTANGLEFLAGS} -L
+NOTANGLE.hs?=       ${NOTANGLE} ${NOTANGLEFLAGS.hs} -R$@ $< | ${CPIF} $@
+NOWEB_SUFFIXES+=    .py
+NOTANGLEFLAGS.py?=  ${NOTANGLEFLAGS}
+NOTANGLE.py?=       ${NOTANGLE} ${NOTANGLEFLAGS.py} -R$@ $< > $@
+NOWEB_SUFFIXES+=    .mk
+NOTANGLEFLAGS.mk?=  ${NOTANGLEFLAGS}
+NOTANGLE.mk?=       ${NOTANGLE} ${NOTANGLEFLAGS.mk} -R$@ $< > $@
+.SUFFIXES: .nw .tex $(addsuffix .nw,${NOWEB_SUFFIXES})
+.nw.tex $(addsuffix .nw.tex,${NOWEB_SUFFIXES}):
+	${NOWEAVE} ${NOWEAVEFLAGS} $< > $@
+%: %.nw
+	${NOTANGLE$(suffix $@)}
+$(addprefix %,${NOWEB_SUFFIXES}): %.nw
+	${NOTANGLE$(suffix $@)}
+
 
 endif
