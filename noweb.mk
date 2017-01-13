90c8ada6c6dff06e7a60c18d3125df010daf4f4b
Daniel Bosk <dbosk@kth.se>
2017-01-13 11:22:38 +0100

    Brings precompiled noweb.mk and portability.mk up to date

--- noweb.mk
+++ noweb.mk
@@ -6,7 +6,24 @@ NOWEAVEFLAGS?=  -x -n -delay -t2
 NOTANGLE?=      notangle
 NOTANGLEFLAGS?= -t2
 CPIF?=          cpif
-
+NOWEB_SUFFIXES+=    .c .cc .cpp .cxx
+NOTANGLEFLAGS.c?=   ${NOTANGLEFLAGS} -L
+NOTANGLE.c?=        ${NOTANGLE} ${NOTANGLEFLAGS.c} -R$@ $< | ${CPIF} $@
+NOTANGLEFLAGS.cc?=  ${NOTANGLEFLAGS.c}
+NOTANGLE.cc?=       ${NOTANGLE} ${NOTANGLEFLAGS.cc} -R$@ $< | ${CPIF} $@
+NOTANGLEFLAGS.cpp?= ${NOTANGLEFLAGS.c}
+NOTANGLE.cpp?=      ${NOTANGLE} ${NOTANGLEFLAGS.cpp} -R$@ $< | ${CPIF} $@
+NOTANGLEFLAGS.cxx?= ${NOTANGLEFLAGS.c}
+NOTANGLE.cxx?=      ${NOTANGLE} ${NOTANGLEFLAGS.cxx} -R$@ $< | ${CPIF} $@
+NOWEB_SUFFIXES+=    .h .hh .hpp .hxx
+NOTANGLEFLAGS.h?=   ${NOTANGLEFLAGS} -L
+NOTANGLE.h?=        ${NOTANGLE} ${NOTANGLEFLAGS.h} -R$@ $< | ${CPIF} $@
+NOTANGLEFLAGS.hh?=  ${NOTANGLEFLAGS.h}
+NOTANGLE.hh?=       ${NOTANGLE} ${NOTANGLEFLAGS.hh} -R$@ $< | ${CPIF} $@
+NOTANGLEFLAGS.hpp?= ${NOTANGLEFLAGS.h}
+NOTANGLE.hpp?=      ${NOTANGLE} ${NOTANGLEFLAGS.hpp} -R$@ $< | ${CPIF} $@
+NOTANGLEFLAGS.hxx?= ${NOTANGLEFLAGS.h}
+NOTANGLE.hxx?=      ${NOTANGLE} ${NOTANGLEFLAGS.hxx} -R$@ $< | ${CPIF} $@
 NOWEB_SUFFIXES+=    .hs
 NOTANGLEFLAGS.hs?=  ${NOTANGLEFLAGS} -L
 NOTANGLE.hs?=       ${NOTANGLE} ${NOTANGLEFLAGS.hs} -R$@ $< | ${CPIF} $@
@@ -16,13 +33,29 @@ NOTANGLE.py?=       ${NOTANGLE} ${NOTANGLEFLAGS.py} -R$@ $< > $@
 NOWEB_SUFFIXES+=    .mk
 NOTANGLEFLAGS.mk?=  ${NOTANGLEFLAGS}
 NOTANGLE.mk?=       ${NOTANGLE} ${NOTANGLEFLAGS.mk} -R$@ $< > $@
+NOWEB_SUFFIXES+=    .sh
+NOTANGLEFLAGS.sh?=  ${NOTANGLEFLAGS}
+NOTANGLE.sh?=       ${NOTANGLE} ${NOTANGLEFLAGS.sh} -R$@ $< > $@
 .SUFFIXES: .nw .tex $(addsuffix .nw,${NOWEB_SUFFIXES})
 .nw.tex $(addsuffix .nw.tex,${NOWEB_SUFFIXES}):
 	${NOWEAVE} ${NOWEAVEFLAGS} $< > $@
-%: %.nw
-	${NOTANGLE$(suffix $@)}
+define with_suffix_target
+%$(1): %$(1).nw
+	$${NOTANGLE$$(suffix $$@)}
+endef
+$(foreach suf,${NOWEB_SUFFIXES},$(eval $(call with_suffix_target,${suf})))
 $(addprefix %,${NOWEB_SUFFIXES}): %.nw
 	${NOTANGLE$(suffix $@)}
+%.h: %.c.nw
+	${NOTANGLE.h}
 
+%.hh: %.cc.nw
+	${NOTANGLE.hh}
+
+%.hpp: %.cpp.nw
+	${NOTANGLE.hpp}
+
+%.hxx: %.cxx.nw
+	${NOTANGLE.hxx}
 
 endif
