64f6866ba3f12c265c27b723cb760be947cbc808
Daniel Bosk <dbosk@kth.se>
2018-01-11 11:05:59 +0100

    Adds new precompiled version of subdir.mk

--- subdir.mk
+++ subdir.mk
@@ -2,14 +2,19 @@ ifndef SUBDIR_MK
 SUBDIR_MK=true
 
 ifdef SUBDIR
+${SUBDIR}::
+	${MAKE} -C $@ ${MAKECMDGOALS}
+endif
+
+SUBDIR_ALL?=yes
+
+ifeq (${SUBDIR_ALL},yes)
 ifneq (${MAKECMDGOALS},)
 .PHONY: ${MAKECMDGOALS}
 ${MAKECMDGOALS}: ${SUBDIR}
 else
 ${.DEFAULT_GOAL}: ${SUBDIR}
 endif
-${SUBDIR}::
-	${MAKE} -C $@ ${MAKECMDGOALS}
 endif
 
 endif
