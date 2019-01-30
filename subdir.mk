d2656dfa372b4d353607acdc71df0aa75329e9f3
Daniel Bosk <dbosk@kth.se>
2019-01-30 11:40:28 +0100

    subdir: breaks out treatment of default goal
    This changes the behaviour back to the old one: if the recursion starts
    with the default goal, the default goal in each subdirectory will be
    executed --- \ie not necessarily the same goal in every subdirectory.

--- subdir.mk
+++ subdir.mk
@@ -2,7 +2,7 @@ ifndef SUBDIR_MK
 SUBDIR_MK=true
 
 SUBDIR_GOALS?=${MAKECMDGOALS}
-actual_goals=$(sort $(filter ${SUBDIR_GOALS},${MAKECMDGOALS} ${.DEFAULT_GOAL))
+actual_goals=$(sort $(filter ${SUBDIR_GOALS},${MAKECMDGOALS}))
 
 ifdef SUBDIR
 ${SUBDIR}::
@@ -13,6 +13,10 @@ ifneq (${SUBDIR_GOALS},)
 ifneq (${actual_goals},)
 .PHONY: ${actual_goals}
 ${actual_goals}: ${SUBDIR}
+endif
+ifneq ($(filter ${.DEFAULT_GOAL},${SUBDIR_GOALS}),)
+.PHONY: ${.DEFAULT_GOAL}
+${.DEFAULT_GOAL}: ${SUBDIR}
 endif
 endif
 
