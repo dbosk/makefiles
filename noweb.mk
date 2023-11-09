67d3dffd90f19941ad8d95058a8c24450b456757
Daniel Bosk <dbosk@kth.se>
2023-11-09 21:12:58 +0100

    Adds noroots to the tangling rules

--- noweb.mk
+++ noweb.mk
@@ -9,48 +9,48 @@ NOWEAVE.pdf?=       \
 NOWEAVEFLAGS.pdf?=  ${NOWEAVEFLAGS} -x -t2
 NOTANGLEFLAGS?=
 NOTANGLE?=      notangle ${NOTANGLEFLAGS} -R$(notdir $@) $(filter %.nw,$^) | \
-                  ${CPIF} $@
+                  ${CPIF} $@ && noroots $(filter %.nw,$^)
 CPIF?=          cpif
 NOWEB_SUFFIXES+=    .c .cc .cpp .cxx
 NOTANGLEFLAGS.c?=   -L
 NOTANGLE.c?=        notangle ${NOTANGLEFLAGS.c} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOTANGLEFLAGS.cc?=  ${NOTANGLEFLAGS.c}
 NOTANGLE.cc?=       notangle ${NOTANGLEFLAGS.cc} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOTANGLEFLAGS.cpp?= ${NOTANGLEFLAGS.c}
 NOTANGLE.cpp?=      notangle ${NOTANGLEFLAGS.cpp} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOTANGLEFLAGS.cxx?= ${NOTANGLEFLAGS.c}
 NOTANGLE.cxx?=      notangle ${NOTANGLEFLAGS.cxx} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOWEB_SUFFIXES+=    .h .hh .hpp .hxx
 NOTANGLEFLAGS.h?=   -L
 NOTANGLE.h?=        notangle ${NOTANGLEFLAGS.h} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOTANGLEFLAGS.hh?=  ${NOTANGLEFLAGS.h}
 NOTANGLE.hh?=       notangle ${NOTANGLEFLAGS.hh} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOTANGLEFLAGS.hpp?= ${NOTANGLEFLAGS.h}
 NOTANGLE.hpp?=      notangle ${NOTANGLEFLAGS.hpp} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOTANGLEFLAGS.hxx?= ${NOTANGLEFLAGS.h}
 NOTANGLE.hxx?=      notangle ${NOTANGLEFLAGS.hxx} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOWEB_SUFFIXES+=    .hs
 NOTANGLEFLAGS.hs?=  -L
 NOTANGLE.hs?=       notangle ${NOTANGLEFLAGS.hs} -R$(notdir $@) \
-  $(filter %.nw,$^) | ${CPIF} $@
+  $(filter %.nw,$^) | ${CPIF} $@ && noroots $(filter %.nw,$^)
 NOWEB_SUFFIXES+=    .mk
 NOTANGLEFLAGS.mk?=  -t2
 NOTANGLE.mk?=       notangle ${NOTANGLEFLAGS.mk} -R$(notdir $@) \
-  $(filter %.nw,$^) > $@
+  $(filter %.nw,$^) > $@ && noroots $(filter %.nw,$^)
 NOWEB_SUFFIXES+=    .py .sty .cls .sh .go
 
 define default_tangling
 NOTANGLEFLAGS$(1)?=
 NOTANGLE$(1)?=      notangle $${NOTANGLEFLAGS$(1)} -R$$(notdir $$@) \
-  $$(filter %.nw,$$^) > $$@
+  $$(filter %.nw,$$^) > $$@ && noroots $$(filter %.nw,$$^)
 endef
 
 $(foreach suffix,${NOWEB_SUFFIXES},$(eval $(call default_tangling,${suffix})))
